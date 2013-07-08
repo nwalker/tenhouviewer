e89d761259a18933cef0abc0cd7861d782796530
yateplofizik@yandex.ru <yateplofizik@yandex.ru@64c22c24-7d2f-7f0e-256c-7402228c70db>
2013-07-08 20:35:41 +0000

    Created classes for all objects

---
+++ trunk/Mahjong/Replay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Mahjong
+{
+    class Replay
+    {
+        private List<Player> Players = new List<Player>();
+        private List<Round> Rounds = new List<Round>();
+
+        public Replay()
+        {
+
+        }
+    }
+}
