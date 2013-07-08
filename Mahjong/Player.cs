744e3d7ec99a51e369350966d23a5685e1c6fa35
yateplofizik <yateplofizik@yandex.ru>
2013-07-08 20:35:41 +0000

    Created classes for all objects

---
+++ Mahjong/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Mahjong
+{
+    class Player
+    {
+        public string NickName = "";
+        public int Rank = 0;
+        public int Rating = 0;
+
+        public Player()
+        {
+
+        }
+    }
+}
---
+++ Mahjong/Round.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Mahjong
+{
+    class Round
+    {
+        private Wall Wall = new Wall();
+        private List<Step> Steps = new List<Step>();
+
+        public Round()
+        {
+            
+
+        }
+    }
+}
