7d6d8a5fa964088b534e77a6a347a1f8ebb3df30
yateplofizik <yateplofizik@yandex.ru>
2013-07-09 11:01:55 +0000

    Parsing UN tag, INIT tag (only start hands)

--- Mahjong/Hand.cs
+++ Mahjong/Hand.cs
@@ -7,9 +7,19 @@ namespace TenhouViewer.Mahjong
 {
     class Hand
     {
+        int[] Tiles = new int[13];
+        int DrowTile;
+
         public Hand()
         {
 
+        }
+
+        public void SetArray(int[] Tiles)
+        {
+            for (int i = 0; i < 13; i++) this.Tiles[i] = Tiles[i];
+
+            Array.Sort(this.Tiles);
         }
     }
 }
