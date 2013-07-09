72ef42c66c7e8de49cc97727015e33fea0d82ffd
yateplofizik <yateplofizik@yandex.ru>
2013-07-09 07:16:14 +0000

    *.mjlog decompression

--- Tenhou/Replay.cs
+++ Tenhou/Replay.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Xml;
@@ -16,15 +18,19 @@ namespace TenhouViewer.Tenhou
         // .xml
         public void OpenPlainText(string Filename)
         {
-            XmlReader reader = XmlReader.Create(Filename);
+            XmlReader Reader = XmlReader.Create(Filename);
 
-            Parse(reader);
+            Parse(Reader);
         }
 
         // .mjlog
         public void OpenGZ(string Filename)
         {
+            FileStream File = new FileStream(Filename, FileMode.Open, FileAccess.Read);
+            GZipStream Stream = new GZipStream(File, CompressionMode.Decompress);
+            XmlReader Reader = XmlReader.Create(Stream);
 
+            Parse(Reader);
         }
 
         private void Parse(XmlReader Reader)
