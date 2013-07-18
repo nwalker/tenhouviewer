fd914dfe13d0a95bbd9eecc28bbaa1c94ea4a22e
yateplofizik@yandex.ru <yateplofizik@yandex.ru@64c22c24-7d2f-7f0e-256c-7402228c70db>
2013-07-18 10:57:12 +0000

    Basic game search engine

---
+++ trunk/Search/GameFinder.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Search
+{
+    class GameFinder
+    {
+        private List<Result> GameList = new List<Result>();
+
+        // Find queries
+        // Fields use if they has not default values
+
+        // NickName
+        public string NickName = null;
+
+        // Yaku (hand contain all yaku from this array)
+        public int[] YakuList = null;
+
+        // Player place
+        public int Place = 0; 
+
+        public GameFinder(Tenhou.TenhouHashList Hashes)
+        {
+            // Create blank ResultList from hash table
+            foreach (string Hash in Hashes.Hashes)
+            {
+                Mahjong.Replay R = new Mahjong.Replay();
+
+                if (R.LoadXml(Hash))
+                {
+                    Result Res = new Result();
+                    Res.Replay = R;
+
+                    for (int i = 0; i < R.Rounds.Count; i++)
+                    {
+                        bool[] Marks = new bool[4];
+                        for(int j = 0; j < 4; j++) Marks[j] = true;
+
+                        Res.RoundMark.Add(true);
+                        Res.HandMark.Add(Marks);
+                    }
+
+                    GameList.Add(Res);
+                }
+                else
+                {
+                    Console.WriteLine(Hash + " - not found");
+                }
+            }
+        }
+
+        // Finder can use results of previous queries
+        public GameFinder(List<Result> Results)
+        {
+            // Reset marks 
+            foreach (Result R in Results)
+            {
+                R.ReplayMark = true;
+
+                for (int i = 0; i < R.RoundMark.Count; i++)
+                {
+                    R.RoundMark[i] = true;
+                    for (int j = 0; j < 4; j++) R.HandMark[i][j] = true;
+                }
+
+                GameList.Add(R);
+            }
+        }
+
+        public List<Result> Find()
+        {
+            List<Result> ResultList = new List<Result>();
+
+            for (int i = 0; i < GameList.Count; i++)
+            {
+                Result R = GameList[i];
+
+                // filters
+                CheckNickName(R);
+                CheckYaku(R);
+                CheckPlace(R);
+
+                // Check mark
+                if (!IsQueryOk(R)) continue;
+
+                EmbedMarksToHandMark(R);
+                ResultList.Add(R);
+            }
+
+            return ResultList;
+        }
+
+        private bool IsQueryOk(Result R)
+        {
+            if (!R.ReplayMark) return false;
+            if (!R.PlayerMark.Contains(true)) return false;
+
+            for (int i = 0; i < R.RoundMark.Count; i++)
+            {
+                // If any round contain positive result...
+                if (R.RoundMark[i] && R.HandMark[i].Contains(true)) return true;
+            }
+
+            return false;
+        }
+
+        private void EmbedMarksToHandMark(Result R)
+        {
+            for (int i = 0; i < R.RoundMark.Count; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (!R.PlayerMark[j]) R.HandMark[i][j] = false;
+                    if (!R.RoundMark[i]) R.HandMark[i][j] = false;
+                }
+            }
+        }
+
+        // filters
+        private void CheckNickName(Result R)
+        {
+            if (NickName == null) return;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (R.Replay.Players[i].NickName.CompareTo(NickName) != 0)
+                    R.PlayerMark[i] = false;
+            }
+        }
+
+        private void CheckPlace(Result R)
+        {
+            if (Place == 0) return;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (R.Replay.Place[i] == Place)
+                    R.PlayerMark[i] = false;
+            }
+        }
+
+        private bool IsYakuInYakuList(List<Mahjong.Yaku> YakuList, int Yaku)
+        {
+            for (int i = 0; i < YakuList.Count; i++)
+            {
+                if(YakuList[i].Index == Yaku) return true;
+            }
+
+            return false;
+        }
+
+        private void CheckYaku(Result R)
+        {
+            if (YakuList == null) return;
+
+            for (int RoundIndex = 0; RoundIndex < R.Replay.Rounds.Count; RoundIndex++ )
+            {
+                Mahjong.Round Rnd = R.Replay.Rounds[RoundIndex];
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < YakuList.Length; j++)
+                    {
+                        if (!IsYakuInYakuList(Rnd.Yaku[i], YakuList[j]))
+                        {
+                            R.HandMark[RoundIndex][i] = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
