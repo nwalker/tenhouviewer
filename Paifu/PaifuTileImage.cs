909dcd8f60355fe6cebc04db5465ba1075aad4cd
yateplofizik <yateplofizik@yandex.ru>
2013-07-23 10:30:30 +0000

    Basic paifu generation support (drawing and discarding tiles, some naki)

---
+++ Paifu/PaifuTileImage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace TenhouViewer.Paifu
+{
+    class PaifuTileImage
+    {
+        Mahjong.Tile Tile;
+        public Bitmap Bmp;
+
+        public PaifuTileImage(int Index, float Scale)
+        {
+            Tile = new Mahjong.Tile(Index);
+
+            string TileName;
+
+            switch(Index)
+            {
+                case -1: // closed tile
+                    TileName = "0z";
+                    break;
+                case -2: // tsumogiri
+                    TileName = "tsumogiri";
+                    break;
+                default:
+                    TileName = Tile.TileName;
+                    break;
+            }
+
+            string FileName = String.Format(".//tiles//{0:s}.gif", TileName);
+            Bmp = new Bitmap(FileName);
+            Bmp = new Bitmap(Bmp);
+        }
+    }
+}
