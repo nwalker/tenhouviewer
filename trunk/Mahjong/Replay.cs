472d3699fcc147f43a1a18819a7c9c08519f79ed
yateplofizik@yandex.ru <yateplofizik@yandex.ru@64c22c24-7d2f-7f0e-256c-7402228c70db>
2013-11-08 07:29:16 +0000

    Bug fix

--- trunk/Mahjong/Replay.cs
+++ trunk/Mahjong/Replay.cs
@@ -68,7 +68,7 @@ namespace TenhouViewer.Mahjong
                 {
                     case "hash": this.Hash = X.GetAttribute("value"); break;
                     case "playercount": PlayerCount = X.GetIntAttribute("value"); break;
-                    case "lobby": Lobby = X.GetIntAttribute("lobby"); break;
+                    case "lobby": Lobby = X.GetIntAttribute("value"); break;
                     case "lobbytype": LobbyType = X.GetIntAttribute("lobbytype"); break;
                     case "balance": Balance = X.ReadIntArray(); break;
                     case "result": Result = X.ReadIntArray(); break;
