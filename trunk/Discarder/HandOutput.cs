07409ba03652d2f02e1fac7f2727df36e4ec4f78
yateplofizik@yandex.ru <yateplofizik@yandex.ru@64c22c24-7d2f-7f0e-256c-7402228c70db>
2013-12-16 13:01:09 +0000

    Update argument description; Add hand output class (empty);

---
+++ trunk/Discarder/HandOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Discarder
+{
+    class HandOutput
+    {
+        public HandOutput(Mahjong.Replay Replay, int Round, int Player)
+        {
+
+        }
+    }
+}
