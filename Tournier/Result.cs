4e47d1c546a6e53fb8608441824bb54e2d8b83d4
yateplofizik <yateplofizik@yandex.ru>
2013-12-16 20:57:05 +0000

    Basic tournier mode support; Calculating average place/total points;

---
+++ Tournier/Result.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Tournier
+{
+    class Result
+    {
+        // Player's places
+        public List<int> Places = new List<int>();
+
+        // Player's points
+        public List<int> Points = new List<int>();
+
+        // Player's total points
+        public int TotalPoints = 0;
+
+        // Player's average place
+        public float AveragePlace = 0.0f;
+
+        // Player's nickname
+        public string NickName = null;
+
+        public Result(string Name)
+        {
+            NickName = Name;
+        }
+
+        public void AddResult(int Place, int Points)
+        {
+            this.Places.Add(Place);
+            this.Points.Add(Points);
+
+            float TempPlace = 0;
+            int TempPoints = 0;
+            for (int i = 0; i < this.Places.Count; i++)
+            {
+                TempPlace += Places[i];
+                TempPoints += this.Points[i];
+            }
+
+            TotalPoints = TempPoints;
+            AveragePlace = TempPlace / this.Places.Count;
+        }
+    }
+}
---
+++ Tournier/Tournier.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Tournier
+{
+    class Tournier
+    {
+        private List<Mahjong.Replay> Replays = new List<Mahjong.Replay>();
+        private List<Result> Results = new List<Result>();
+
+        public Tournier(Tenhou.TenhouHashList Hashes)
+        {
+            // Build result list
+            for (int h = 0; h < Hashes.Hashes.Count; h++)
+            {
+                string Hash = Hashes.Hashes[h];
+                Mahjong.Replay R = new Mahjong.Replay();
+
+                Console.Title = String.Format("Loading {0:d}/{1:d}", h + 1, Hashes.Hashes.Count);
+
+                if (R.LoadXml(Hash))
+                {
+                    Replays.Add(R);
+                }
+            }
+        }
+
+        public void Analyze()
+        {
+            // Analyze player's results
+            for (int i = 0; i < Replays.Count; i++)
+            {
+                Mahjong.Replay R = Replays[i];
+
+                for (int p = 0; p < R.PlayerCount; p++)
+                {
+                    Result Res = GetPlayerResult(R.Players[p].NickName);
+
+                    Res.AddResult(R.Place[p], R.Balance[p]);
+                }
+            }
+        }
+
+        private Result GetPlayerResult(string Name)
+        {
+            for (int i = 0; i < Results.Count; i++)
+            {
+                Result R = Results[i];
+
+                if (R.NickName.CompareTo(Name) == 0)
+                {
+                    // Player already exists in table
+                    return R;
+                }
+            }
+
+            // Not found. Create new:
+            Result NewResult = new Result(Name);
+            Results.Add(NewResult);
+
+            return NewResult;
+        }
+    }
+}
