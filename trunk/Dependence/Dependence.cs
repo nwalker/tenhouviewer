e29917449ec6b8315c18c87e963c42abda6d4055
yateplofizik@yandex.ru <yateplofizik@yandex.ru@64c22c24-7d2f-7f0e-256c-7402228c70db>
2013-12-23 13:32:10 +0000

    Basic dependence module;

---
+++ trunk/Dependence/Dependence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Dependence
+{
+    class Dependence
+    {
+        private List<Search.Result> Results = null;
+
+        public Dependence(List<Search.Result> Results)
+        {
+            this.Results = Results;
+        }
+    }
+}
---
+++ trunk/Dependence/Value.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TenhouViewer.Dependence
+{
+    class Value
+    {
+        List<float> Buffer = new List<float>();
+
+        public Value()
+        {
+
+        }
+
+        public void Add(float Value)
+        {
+            Buffer.Add(Value);
+        }
+
+        public float Minimum
+        {
+            get
+            {
+                if (Buffer.Count == 0) return 0;
+
+                float Min = Buffer[0];
+                foreach (float T in Buffer)
+                {
+                    if (T < Min) Min = T;
+                }
+
+                return Min;
+            }
+        }
+
+        public float Maximum
+        {
+            get
+            {
+                if (Buffer.Count == 0) return 0;
+
+                float Max = Buffer[0];
+                foreach (float T in Buffer)
+                {
+                    if (T > Max) Max = T;
+                }
+
+                return Max;
+            }
+        }
+
+        public float Average
+        {
+            get
+            {
+                float Avg = 0.0f;
+                foreach (float T in Buffer)
+                {
+                    Avg += T;
+                }
+
+                return (Buffer.Count > 0) ? (Avg / Buffer.Count) : 0;
+            }
+        }
+    }
+}
